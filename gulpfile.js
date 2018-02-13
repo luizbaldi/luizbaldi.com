69681aa65a9e633461a8fc0bd49df03810bee7e9
Luiz Baldi <baldilp@gmail.com>
2018-02-13 11:06:08 -0200

    Add gulp and remove static lib files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const useref = require('gulp-useref');
+const cssnano = require('gulp-cssnano');
+const gulpIf = require('gulp-if');
+
+gulp.task('useref', () => {
+  return gulp.src('index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('fontawesome', () => {
+  return gulp.src('node_modules/font-awesome/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('default', ['useref', 'fontawesome', 'images', 'fonts']);
